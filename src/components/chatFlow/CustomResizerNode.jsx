84e100e8400b3d8836737e4e93453b660462a18a
a7ridi <a7ridi@gmail.com>
2024-06-01 23:05:41 +0530

    added whatsapp icon

--- src/components/chatFlow/CustomResizerNode.jsx
+++ src/components/chatFlow/CustomResizerNode.jsx
@@ -1,12 +1,18 @@
 import { memo } from "react";
 import { Handle, Position } from "reactflow";
+import WhatsAppIcon from "../../images/whatsapp.svg";
+import MessageIcon from "../../images/message.svg";
 
 const CustomNode = ({ data, selected }) => {
   return (
     <div className={`${selected ? "border-2 border-blue-600 rounded-lg" : ""}`}>
       <Handle type="target" position={Position.Left} />
-      <div className="bg-green-200 py-1 px-3 rounded-t-lg font-bold">
-        Send message
+      <div className="bg-green-200 py-1 px-3 rounded-t-lg font-bold flex justify-between">
+        <div className="flex gap-x-2">
+          <img src={MessageIcon} alt="message-icon" width={12} height={12} />
+          <h6>Send message</h6>
+        </div>
+        <img src={WhatsAppIcon} alt="whatsapp-icon" width={20} height={20} />
       </div>
       <div className="my-2 px-4">{data.label}</div>
       <Handle type="source" position={Position.Right} />
